c6858e6792323cbfd07c8c97dc97f33424f469b5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-05-21 17:38:08 -0300

    feat(normalizer): add helper module to sanitize variable values

---
+++ src/helpers/normalizer.py
@@ -0,0 +1,27 @@
+''' Module to normalize strings '''
+import unicodedata
+
+def norm_txt(text: str) -> str:
+    ''' Function to normalize strings '''
+    text = text.strip().casefold()
+    text = unicodedata.normalize('NFD', text)
+    text = ''.join(
+        c for c in text
+        if unicodedata.category(c) != 'Mn'
+    )
+    return text
+
+def norm_float(number: float | int) -> str:
+    ''' Function to normalize float '''
+    return f'{number:.2f}'.replace('.', ',')
+
+def norm_money(value: str) -> float:
+    ''' Function to convert brazilian monetary string to float '''
+    return float(
+        value
+            .replace('R$', '')
+            .replace('%', '')
+            .replace('.', '')
+            .replace(',', '.')
+            .strip()
+        )
